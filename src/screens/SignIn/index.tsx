6b18511eca981ac603c218a152ec77d759b66b45
elias alexandre <elliasallex@gmail.com>
2022-06-20 19:25:22 -0300

    splash screen styles

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StatusBar } from "react-native";
 
 import { styles } from "./styles";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+
+import IllustrationPNG from "../../assets/illustration.png";
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Olá, mundo</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        style={styles.image}
+        source={IllustrationPNG}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {"\n"}
+          Suas Jogatinas {"\n"}
+          Facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {"\n"}
+          favoritos com seus amigos!
+        </Text>
+
+        <ButtonIcon activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,32 @@
 import { StyleSheet } from "react-native";
 
+import { theme } from "../../global/styles/theme";
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.colors.secondary100,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: 360,
+  },
+  content: {
+    marginTop: -40,
+    paddingHorizontal: 30,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 40,
+    marginBottom: 16,
+    color: theme.colors.heading,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 15,
+    marginBottom: 64,
+    color: theme.colors.heading,
   },
 });
