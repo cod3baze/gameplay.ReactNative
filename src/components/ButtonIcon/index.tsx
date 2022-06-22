8fa2bfc692eb3255f94509a8340049e73b0044b6
elias alexandre <elliasallex@gmail.com>
2022-06-22 16:58:18 -0300

    buttonIcon From TouchableOpacity To RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,28 +1,23 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { View, Text, Image } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
 
 import DiscordPNG from "../../assets/discord.png";
 
-type ButtonWithIconProps = TouchableOpacityProps & {
+type ButtonWithIconProps = RectButtonProps & {
   text?: string;
 };
 
 export function ButtonIcon({ text, ...rest }: ButtonWithIconProps) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordPNG} style={styles.icon} />
       </View>
 
       <Text style={styles.title}>{text || "Entrar com discord"}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
